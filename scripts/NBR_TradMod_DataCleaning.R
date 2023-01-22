308111e3b595660f19e437739f9d8c6fb481d40e
MorganeKerdoncuff <morgane.kerdoncuf@uib.no>
2023-01-22 13:13:02 +0100

    Site info raw cleaning - NA in municipality replaced & start check farmer names

--- scripts/NBR_TradMod_DataCleaning.R
+++ scripts/NBR_TradMod_DataCleaning.R
@@ -63,17 +63,19 @@ siteinfo_raw <- subset(siteinfo_raw, select = -c(Size_livestock, Surface)) # rem
 # table(siteinfo_raw$SiteID) # Unique ID for each site - validated
 # unique(siteinfo_raw$NiBioEcologicalZone) # Three categories for ecological zone - validated
 # unique(siteinfo_raw$Location) # Correct locations and location names
-unique(siteinfo_raw$Municipality) # missing data + US2 Stordalen -> did you guys went that far ? Yes validated
-siteinfo_raw[is.na(siteinfo_raw$Municipality),] # NA identified - Stordalen is located in Masfjorden
-
-unique(Site_Info_raw$Farmer) #needs full name Dan
-#Site_Info_raw[is.na(Site_Info_raw$Farmer)]
-#unique(Site_Info_raw$Type_livestock)
-Site_Info_raw <- Site_Info_raw %>% 
+# unique(siteinfo_raw$Municipality) # missing data + US2 Stordalen -> did you guys went that far ? Yes validated
+# siteinfo_raw[is.na(siteinfo_raw$Municipality),] # NA identified - US2, Stordalen is located in Masfjorden
+siteinfo_raw <- siteinfo_raw |> 
+  mutate(Municipality=ifelse(SiteID == "US2", "Masfjorden", Municipality)) # Replace NA by municipality name
+# unique(siteinfo_raw$Municipality) # NA in Municipality replaced - validated
+unique(siteinfo_raw$Farmer) # Missing two full names and one NA
+#siteinfo_raw[is.na(siteinfo_raw$Farmer)]
+#unique(siteinfo_raw$Type_livestock)
+siteinfo_raw <- siteinfo_raw %>% 
   mutate(Type_livestock = dplyr::recode(Type_livestock, "Villsau" = "Sheep")) # Villsau as sheep category
-table(Site_Info_raw$Type_livestock) #10 cows, 11 goats, 23 sheep -> ask Amy again about UG1 & UG2
-#unique(Site_Info_raw$Habitat) #needs to standardise categories, no more than 5 - burnt/non-burnt? alpine/subalpine? 
-#unique(Site_Info_raw$Animal_on_site)
+table(siteinfo_raw$Type_livestock) #10 cows, 11 goats, 23 sheep -> ask Amy again about UG1 & UG2
+#unique(siteinfo_raw$Habitat) #needs to standardise categories, no more than 5 - burnt/non-burnt? alpine/subalpine? 
+#unique(siteinfo_raw$Animal_on_site)
 
 #Binding geology
 Geol_raw <- read_xlsx("..\\06_SurveyDataSets\\Tradmod site geology.xlsx")
