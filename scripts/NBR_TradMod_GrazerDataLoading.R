7e71836b0f9e3bfe6421c6d46204fb5c256017e8
MorganeKerdoncuff <morgane.kerdoncuf@uib.no>
2023-02-14 14:54:15 +0100

    Canonical correlation assumptions - variable colinearity and linear relationships between variables

--- scripts/NBR_TradMod_GrazerDataLoading.R
+++ scripts/NBR_TradMod_GrazerDataLoading.R
@@ -10,8 +10,10 @@
 
 library(tidyverse) # R language
 library(purrr) # Data manipulation: function "reduce" to bind several tables at the same time
-
-
+library(vegan) # Community ecology analysis
+library(ggplot2) # Visual representation
+library(GGally) # Extension ggplot
+library(car) # Visualisation lm residuals plots
 
 #### DATA LOADING ####
 
@@ -145,19 +147,18 @@ contin_beetle <- xtabs(formula = BeetleFam_logabundance ~ SiteID + BeetleFam, da
 ## Aspect angle (num), from area 20x20
 ## Distance to sea (num), from area 20x20
 
-# Selection variables
+# Selection variables & scaling
 fjordsys <- subset(area20x20_grass, select = c(SiteID, Elevation_max, General_slope, AspectDegree, DistanceToSea_m))
+fjordsys <- fjordsys |> 
+  mutate(across(where(is.numeric), scale))
 
 # Contingency table
-fjordsys <- fjordsys |> 
+fjordsys_long <- fjordsys |> 
   pivot_longer(
     cols = c(Elevation_max, General_slope, AspectDegree, DistanceToSea_m),
     names_to = "Factors",
     values_to = "Values")
-contin_fjordsys <- xtabs(formula = Values ~ SiteID + Factors, data = fjordsys)
-
-# Data scaling
-contin_fjordsys <- scale(contin_fjordsys)
+contin_fjordsys <- xtabs(formula = Values ~ SiteID + Factors, data = fjordsys_long)
 
 #
 ## Creation landscape matrix
@@ -174,19 +175,18 @@ landscape_grass <- landscape_grass |>
   mutate(TotCultivatedLand_percent = FullyCultivatedLand_percent + SuperficiallyCultivatedLand_percent) |> 
   mutate(TotForest_percent = ProductiveForest_percent + NonProductiveForest_percent)
 
-# Selection variables
+# Selection variables & scaling
 landscape <- subset(landscape_grass, select = c(SiteID, TotCultivatedLand_percent, TotForest_percent, Infield_percent, Outfield_percent, Wetland_percent))
+landscape <- landscape |> 
+  mutate(across(where(is.numeric), scale))
 
 # Contingency table
-landscape <- landscape |> 
+landscape_long <- landscape |> 
   pivot_longer(
     cols = c(TotCultivatedLand_percent, TotForest_percent, Infield_percent, Outfield_percent, Wetland_percent),
     names_to = "Factors",
     values_to = "Values")
-contin_landscape <- xtabs(formula = Values ~ SiteID + Factors, data = landscape)
-
-# Data scaling
-contin_landscape <- scale(contin_landscape)
+contin_landscape <- xtabs(formula = Values ~ SiteID + Factors, data = landscape_long)
 
 #
 ## Creation grazing management matrix
@@ -203,19 +203,18 @@ landuse_grass <- landuse_grass |>
   mutate(Sheep = ifelse(Livestock1 == "sheep",1,0)) |> 
   mutate(Cow = ifelse(Livestock1 == "cow", 1,0))
 
-# Selection variables
+# Selection variables & scaling
 grazing <- subset(landuse_grass, select = c(SiteID, Sheep, Cow, FlockSize1_adults, GrazingSurface_ha, TotalInfieldSurface, Grazingdensity_perha))
+grazing <- grazing |> 
+  mutate(across(where(is.numeric), scale))
 
 # Contingency table
-grazing <- grazing |> 
+grazing_long <- grazing |> 
   pivot_longer(
     cols = c(Sheep, Cow, FlockSize1_adults, GrazingSurface_ha, TotalInfieldSurface, Grazingdensity_perha),
     names_to = "Factors",
     values_to = "Values")
-contin_grazing <- xtabs(formula = Values ~ SiteID + Factors, data = grazing)
-
-# Data scaling
-contin_grazing <- scale(contin_grazing)
+contin_grazing <- xtabs(formula = Values ~ SiteID + Factors, data = grazing_long)
 
 #
 ## Creation plant local environment matrix
@@ -230,20 +229,19 @@ contin_grazing <- scale(contin_grazing)
 ## pH (num), from soilchem
 ## Humus content (num), from soilchem
 
-# Selection variables
+# Selection variables & scaling
 plantenvi <- purrr::reduce(list(soilbulk_grass, soilpene_grass, soilchem_grass), dplyr::left_join)
 plantenvi <- subset(plantenvi, select = -c(MeanPotassium, MeanSodium))
+plantenvi <- plantenvi |> 
+  mutate(across(where(is.numeric), scale))
 
 # Contingency table
-plantenvi <- plantenvi |> 
+plantenvi_long <- plantenvi |> 
   pivot_longer(
     cols = c(MeanBD, MeanMoisture, MeanPT, MeanLOI, MeanHumus, MeanpH, MeanPhosphorus, MeanNitrogen),
     names_to = "Factors",
     values_to = "Values")
-contin_plantenvi <- xtabs(formula = Values ~ SiteID + Factors, data = plantenvi)
-
-# Data scaling
-contin_plantenvi <- scale(contin_plantenvi)
+contin_plantenvi <- xtabs(formula = Values ~ SiteID + Factors, data = plantenvi_long)
 
 #
 ## Creation beetle local environment matrix
@@ -257,22 +255,120 @@ contin_plantenvi <- scale(contin_plantenvi)
 ## Moisture content (num), from soilbulk
 ## Humus content (num), from soilchem
 
-# Selection variables
+# Selection variables & scaling
 beetlenvi <- purrr::reduce(list(groundcover_grass, soilbulk_grass, soilchem_grass), dplyr::left_join)
 beetlenvi <- beetlenvi |> 
   mutate(MeanExposedGround = MeanBareSoil + MeanRocks)
 beetlenvi <- subset(beetlenvi, select = c(SiteID, MeanExposedGround, MeanLitter, MeanHeight, MeanRichness, MeanBD, MeanMoisture, MeanHumus))
+beetlenvi <- beetlenvi |> 
+  mutate(across(where(is.numeric), scale))
 
 # Contingency beetlenvi
-beetlenvi <- beetlenvi |> 
+beetlenvi_long <- beetlenvi |> 
   pivot_longer(
     cols = c(MeanExposedGround, MeanLitter, MeanHeight, MeanRichness, MeanBD, MeanMoisture, MeanHumus),
     names_to = "Factors",
     values_to = "Values")
-contin_beetlenvi <- xtabs(formula = Values ~ SiteID + Factors, data = beetlenvi)
+contin_beetlenvi <- xtabs(formula = Values ~ SiteID + Factors, data = beetlenvi_long)
 
-# Data scaling
-contin_beetlenvi <- scale(contin_beetlenvi)
+
+#### VERIFICATION ASSUMPTIONS ####
 
 #
-## Check variable colinearity
+## Variables colinearity
+
+# Function for paired panels
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...) {
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  Cor <- abs(cor(x, y)) # Remove abs function if desired
+  txt <- paste0(prefix, format(c(Cor, 0.123456789), digits = digits)[1])
+  if(missing(cex.cor)) {
+    cex.cor <- 0.4 / strwidth(txt)
+  }
+  text(0.5, 0.5, txt,
+       cex = 1 + cex.cor * Cor) # Resize the text by level of correlation
+}
+
+# Fjord system variables
+#paircor_fjordsys <- ggpairs(select_if(fjordsys, is.numeric))
+#paircor_fjordsys # no colinearity between variables, one outlier on distance to sea
+pairs(select_if(fjordsys, is.numeric),
+      upper.panel = panel.cor,
+      lower.panel = panel.smooth) # no colinearity between variables -> validated
+
+# Landscape variables
+#paircor_landscape <- ggpairs(select_if(landscape, is.numeric))
+#paircor_landscape # one correlation a bit strong between total cultivated land and infield percent
+pairs(select_if(landscape, is.numeric),
+      upper.panel = panel.cor,
+      lower.panel = panel.smooth) # one correlation a bit strong (0.74) between total cultivated land and infield percent
+
+# Grazing variables
+pairs(select_if(grazing, is.numeric),
+      upper.panel = panel.cor,
+      lower.panel = panel.smooth) # no correlation stronger than 0.64 -> validated
+
+# Local plant environmental variables
+pairs(select_if(plantenvi, is.numeric),
+      upper.panel = panel.cor,
+      lower.panel = panel.smooth) # 5 variables highly correlated (>0.8): BD, moisture, LOI, humus, Nitrogen
+
+# Local beetle environmental variables
+pairs(select_if(beetlenvi, is.numeric),
+      upper.panel = panel.cor,
+      lower.panel = panel.smooth) # 3 variables highly correlated (>0.8): BD, moisture & humus
+
+# Removal strongly correlated variables - BD kept as with higher number of replication for each site
+plantenvi <- subset(plantenvi, select = -c(MeanMoisture, MeanLOI, MeanHumus, MeanNitrogen))
+beetlenvi <- subset(beetlenvi, select = -c(MeanMoisture, MeanHumus))
+
+#
+## Linear relationships of residuals
+
+# Ordination community data
+DCA_vege <- decorana(contin_vege)
+DCA_vege # Axis length of 3.5 -> keep DCA
+DCA_beetle <- decorana(contin_beetle)
+DCA_beetle # Axis length under 1 -> run PCA
+PCA_beetle <- prcomp(contin_beetle)
+
+# Extraction plant species scores from DCA
+scores_vege <- as.data.frame(scores(DCA_vege, choices = c(1,2), display = "sites"))
+names(scores_vege) <- gsub("DCA1", "VegDCA1", names(scores_vege))
+names(scores_vege) <- gsub("DCA2", "VegDCA2", names(scores_vege))
+scores_vege <- scores_vege |>  
+  mutate(SiteID = row.names(scores_vege)) # PlotID as column for future binding
+
+# Extraction beetle family scores from PCA
+scores_beetle <- as.data.frame(scores(PCA_beetle, choices = c(1,2), display = "sites"))
+names(scores_beetle) <- gsub("PC1", "BeetlePCA1", names(scores_beetle))
+names(scores_beetle) <- gsub("PC2", "BeetlePCA2", names(scores_beetle))
+scores_beetle <- scores_beetle |>  
+  mutate(SiteID = row.names(scores_beetle)) # PlotID as column for future binding
+
+# Table binding
+allvar_vege <- purrr::reduce(list(scores_vege, fjordsys, landscape, grazing, plantenvi), dplyr::left_join)
+allvar_beetle <- purrr::reduce(list(scores_beetle, fjordsys, landscape, grazing, beetlenvi), dplyr::left_join)
+
+# Residuals LM for plants
+residualPlots(lm(VegDCA1~Elevation_max, data = allvar_vege))
+residualPlots(lm(VegDCA1~General_slope, data = allvar_vege))
+residualPlots(lm(VegDCA1~AspectDegree, data = allvar_vege))
+residualPlots(lm(VegDCA1~DistanceToSea_m, data = allvar_vege))
+residualPlots(lm(VegDCA1~TotCultivatedLand_percent, data = allvar_vege))
+residualPlots(lm(VegDCA1~TotForest_percent, data = allvar_vege))
+residualPlots(lm(VegDCA1~Infield_percent, data = allvar_vege))
+residualPlots(lm(VegDCA1~Outfield_percent, data = allvar_vege))
+residualPlots(lm(VegDCA1~Wetland_percent, data = allvar_vege))
+residualPlots(lm(VegDCA1~Sheep, data = allvar_vege))
+residualPlots(lm(VegDCA1~Cow, data = allvar_vege))
+residualPlots(lm(VegDCA1~FlockSize1_adults, data = allvar_vege))
+residualPlots(lm(VegDCA1~GrazingSurface_ha, data = allvar_vege))
+residualPlots(lm(VegDCA1~TotalInfieldSurface, data = allvar_vege))
+residualPlots(lm(VegDCA1~Grazingdensity_perha, data = allvar_vege))
+residualPlots(lm(VegDCA1~MeanBD, data = allvar_vege))
+residualPlots(lm(VegDCA1~MeanPT, data = allvar_vege))
+residualPlots(lm(VegDCA1~MeanpH, data = allvar_vege))
+residualPlots(lm(VegDCA1~MeanPhosphorus, data = allvar_vege))
