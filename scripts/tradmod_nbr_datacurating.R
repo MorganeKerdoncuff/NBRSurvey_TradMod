dc2f91a3c8eaaf1adf2f944a4e4b27423deb49f6
MorganeKerdoncuff <morgane.kerdoncuff@pm.me>
2026-06-16 10:00:29 +0200

    initial commit

---
+++ scripts/tradmod_nbr_datacurating.R
@@ -0,0 +1,20 @@
+##
+#### DESCRIPTION ####
+##
+## Purpose: Data curating script for the ecological survey led in summers 2019 and 2020 in the Nordhordland UNESCO Biosphere Reserve
+## Author: Morgane KERDONCUFF
+## ORCID: 0000-0003-2223-1857
+## github
+## Date created: 06/2026
+## Last time modified: 06/2026
+## Project: TradMod
+## Funding: Norges forskningsråd (NFR)
+## Institution: University of Bergen, Norway
+##
+##
+
+
+#### PACKAGES ####
+
+library(tidyverse) # R language
+library(purrr) # Merge tables
